9f1d5bf5e0c46a587f8780154fe628af9092a0ca
yasir19 <yasir.habboo@gmail.com>
2021-12-15 01:49:06 -0600

    working product with one result

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
-var apiKey ="5d1bf1cd99bc77abc1a5d1777f514808"
-var getWaetherInfo =function (city){
-var apiUrl="https://api.openweathermap.org/data/2.5/weather?q=Houston&units=imperial&appid="+apiKey;
+var resultEl = document.getElementById("result");
+var cityFormEl=document.getElementById("city-form");
+var cityInputEl=document.getElementById("cityname");
+var searchBtn =document.getElementById("searchBTn");
+    var apiKey ="5d1bf1cd99bc77abc1a5d1777f514808"
+var getCity =function(event){
+    event.preventDefault();
+    var cityName=cityInputEl.value.trim();
+    if(cityName){
+        getWaetherInfo(cityName);
+        cityInputEl.value="";
+    }else{
+        alert("please enter a valid city")
+    }
+}
+var getWaetherInfo =function (city,data){
+var apiUrl="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid="+apiKey;
 //make a request to the url 
 fetch(apiUrl).then(function(response){
     if (response.ok){
@@ -16,12 +30,26 @@ fetch(apiUrl).then(function(response){
     alert("Unable to connect to the server")
 });
 };
-getWaetherInfo();
+
+    var nameEl =document.createElement("h3");
+    nameEl.setAttribute("class","city-name");
+    var iconEl =document.createElement("img");
+    iconEl.setAttribute("src","");
+    var tempEl= document.createElement("p");
+    var humidityEl= document.createElement("p");
+    var windEl= document.createElement("p");
+    resultEl.append(nameEl,iconEl,tempEl,humidityEl,windEl);
+ 
 var displayWeather = function(data){
-    var {name}=data;
+    var {name} = data;
     var {icon}=data.weather[0];
     var {temp,humidity}=data.main;
-    var {speed}=data.wind;
-    console.log(name,icon,temp,humidity,speed)
-
-}
+    console.log(name,icon,temp,humidity)
+    nameEl.innerText="Weather in " + name;
+    iconEl.src="http://openweathermap.org/img/wn/" + icon + "@2x.png"
+    tempEl.innerText="Temp: "+ temp + "°F";
+    humidityEl.innerText="Humidity "+ humidity;
+    
+};
+getWaetherInfo("london");
+searchBtn.addEventListener("click", getCity);
